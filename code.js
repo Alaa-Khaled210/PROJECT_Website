23d0a6d61ce0f87560b2ecffd1d08b5ed6ff09f1
Alaa-Khaled210 <alaa.khaled.ramadan@gmail.com>
2026-06-17 17:07:47 +0200

    Refactor authentication flow using localStorage and update profile page

--- code.js
+++ code.js
@@ -1,171 +1,9 @@
-// function store(){
-//     let email = document.getElementById('email');
-//     let password = document.getElementById('password');
-//     let firstName=document.getElementById("fist_name");
-//     let laststName=document.getElementById("last_name");
-
-//     let lowerCaseLetters = /[a-z]/g;
-//     let upperCaseLetters = /[A-Z]/g;
-//     let numbers = /[0-9]/g;
-
-//     if(email.value.length == 0){
-//         alert('Please fill in email');
-
-//     }else if(password.value.length == 0){
-//         alert('Please fill in password');
-
-//     }else if(email.value.length == 0 && password.value.length == 0){
-//         alert('Please fill in email and password');
-
-//     }else if(password.value.length > 8){
-//         alert('Max of 8');
-
-//     }else if(!password.value.match(numbers)){
-//         alert('please add 1 number');
-
-//     }else if(!password.value.match(upperCaseLetters)){
-//         alert('please add 1 uppercase letter');
-
-//     }else if(!password.value.match(lowerCaseLetters)){
-//         alert('please add 1 lowercase letter');
-
-//     }else{
-//         localStorage.setItem('email', email.value);
-//         localStorage.setItem('pw', password.value);
-//         localStorage.setItem('fist_name',firstName.value);
-//         localStorage.setItem('last_name', laststName.value);
-//         alert('Your account has been created');
-//     }
-// }
-
-
-// //this checking login :)
-// function check(){
-//     let storedEmail = localStorage.getItem('email');
-//     let storedPw = localStorage.getItem('password');
-
-//     let Login_email = document.getElementById('Login_email');
-//     let Login_password = document.getElementById('Login_password');
-//     let userRemember = document.getElementById("rememberMe");
-
-//     if(Login_email.value == storedEmail && Login_password.value == storedPw){
-//         alert('You are logged in.');
-
-      
-//     }else{
-//         alert('Error on login');
-
-//     }
-// }
-
-
-// //----------------cookies---------------------//
-
-
-// function setCookie(cname, cvalue, exdays) {
-//   const d = new Date();
-//   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-//   let expires = "expires="+d.toUTCString();
-//   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=.";
-// }
-
-// function getCookie(cname) {
-//   let name = cname + "=";
-//   let ca = document.cookie.split(';');
-//   for(let i = 0; i < ca.length; i++) {
-//     let c = ca[i];
-//     while (c.charAt(0) == ' ') {
-//       c = c.substring(1);
-//     }
-//     if (c.indexOf(name) == 0) {
-//       return c.substring(name.length, c.length);
-//     }
-//   }
-//   return "";
-// }
-
-
-// function checkCookie() {
-//   let user = getCookie("username");
-//   if (user != "") {
-//     alert("Welcome again " + user);
-//   } else {
-//       // try to make him take the name from the text field
-//   //    user = prompt("Please enter your name:","");
-//      user = document.getElementById("Login_email").value;
-
-//      if (user != "" && user != null) {
-//        setCookie("username", user, 30);
-//      }
-//   }
-// }
-
-
-// function deleteCookie() {
-//   const cookies = document.cookie.split(";");
-
-//   for (let i = 0; i < cookies.length; i++) {
-//       const cookie = cookies[i];
-//       const eqPos = cookie.indexOf("=");
-//       const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-//       document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-//       alert("cookie is deleted");
-//   }
-// }
-
-// function edit()
-// {
-//   // can i put them global ?!
-//   // let email = document.getElementById('email');
-//   // let password = document.getElementById('password');
-//   // let firstName=document.getElementById("fist_name");
-//   // let laststName=document.getElementById("last_name");
-//   // //////
-//   let storedEmail = localStorage.getItem('email');
-//   let storedPW = localStorage.getItem('password');
-//   let storedFname=localStorage.getItem("fist_name");
-//   let storedLname=localStorage.getItem("last_name");
-
-
-//   let lowerCaseLetters = /[a-z]/g;
-//   let upperCaseLetters = /[A-Z]/g;
-//   let numbers = /[0-9]/g;
-
-//   document.getElementById('email').value= storedEmail ;
-//   document.getElementById('password').value= storedPW ;
-//   document.getElementById('fist_name').value= storedFname ;
-//   document.getElementById('last_name').value= storedLname ;
-
-
-//   alert('Your account has been updated successfully ;)');
-// }
-//       //---------------------HOME------------------------------//
-
-//  let hamburger = document.querySelector('.hamburger');
-//   let navLinks = document.getElementById('nav-links');
-//  let links = document.querySelectorAll('.links');
-      
-//   hamburger.addEventListener('click', () => {
-//     navLinks.classList.toggle('hide');
-//    hamburger.classList.toggle('lines-rotate');
-//      });
-      
-//  for (let i = 0; i < links.length; i++) {
-//     links[i].addEventListener('click', () => {
-//      navLinks.classList.toggle('hide');
-//         });
-//       }
-
-
-
-
-
 
 
 //----------------cookies---------------------//
 
 
-function setCookie(cname, cvalue, exdays) {
+/* function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   let expires = "expires="+d.toUTCString();
@@ -214,17 +52,17 @@ function deleteCookie() {
       document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
       alert("cookie is deleted");
   }
-}
+} */
 //////////////////////////////////////////////////////////////////////////////////////////////////
 function inerror_case(){
   let email = document.getElementById('email');
   let password = document.getElementById('password');
   let firstName=document.getElementById("fist_name");
   let laststName=document.getElementById("last_name");
-  email="";
-  password="";
-  firstName="";
-  laststName="";
+email.value = "";
+password.value = "";
+firstName.value = "";
+laststName.value = "";
 
 }
 
@@ -239,19 +77,19 @@ function store(){
     let upperCaseLetters = /[A-Z]/g;
     let numbers = /[0-9]/g;
 
-    if(email.value.length == 0){
-        alert('Please fill in email');
+    if(email.value.length == 0 && password.value.length == 0){
+        alert('Please fill in email and password');
         
+
+    }else if(email.value.length == 0){
+        alert('Please fill in email');
+      
 
     }else if(password.value.length == 0){
         alert('Please fill in password');
-      
-
-    }else if(email.value.length == 0 && password.value.length == 0){
-        alert('Please fill in email and password');
    
-    }else if(password.value.length > 8){
-        alert('Max of 8');
+    }else if(password.value.length < 8){
+        alert('Must be at least 8 characters');
        
 
     }else if(!password.value.match(numbers)){
@@ -270,7 +108,7 @@ function store(){
         localStorage.setItem('pw', password.value);
         localStorage.setItem('fist_name',firstName.value);
         localStorage.setItem('last_name', laststName.value);
-        alert('Your account has been created');
+        alert('Profile updated successfully');
     }
 }
 
@@ -285,7 +123,11 @@ function check(){
     let userRemember = document.getElementById("rememberMe");
 
     if(Login_email.value == storedEmail && Login_password.value == storedPw){
+        localStorage.setItem("isLoggedIn", "true");
+
         alert('You are logged in.');
+
+        window.location.href = "index.html"
         return true;
        
     }else{
@@ -295,7 +137,29 @@ function check(){
     }
 }
 
+function logout(){
+    localStorage.removeItem("isLoggedIn");
+    window.location.href = "login.html";
+}
 
+function updateNavbar(){
+
+    let isLoggedIn = localStorage.getItem("isLoggedIn");
+
+    let loginBtn = document.getElementById("loginBtn");
+    let registerBtn = document.getElementById("registerBtn");
+    let logoutBtn = document.getElementById("logoutBtn");
+
+    if(isLoggedIn === "true"){
+        if(loginBtn) loginBtn.style.display = "none";
+        if(registerBtn) registerBtn.style.display = "none";
+        if(logoutBtn) logoutBtn.style.display = "inline-block";
+    }else{
+        if(loginBtn) loginBtn.style.display = "inline-block";
+        if(registerBtn) registerBtn.style.display = "inline-block";
+        if(logoutBtn) logoutBtn.style.display = "none";
+    }
+}
 
 
 
@@ -315,7 +179,7 @@ function edit()
   // let laststName=document.getElementById("last_name");
   // //////
   let storedEmail = localStorage.getItem('email');
-  let storedPW = localStorage.getItem('password');
+  let storedPW = localStorage.getItem('pw');
   let storedFname=localStorage.getItem("fist_name");
   let storedLname=localStorage.getItem("last_name");
 
@@ -330,9 +194,8 @@ function edit()
   document.getElementById('last_name').value= storedLname ;
 
 
-  alert('Your account has been updated successfully ;)');
+ // alert('Your account has been updated successfully ;)');
 }
-
 
 
       //---------------------HOME------------------------------//
@@ -341,6 +204,7 @@ function edit()
   let navLinks = document.getElementById('nav-links');
  let links = document.querySelectorAll('.links');
       
+ if (hamburger && navLinks){
   hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('hide');
    hamburger.classList.toggle('lines-rotate');
@@ -350,9 +214,7 @@ function edit()
     links[i].addEventListener('click', () => {
      navLinks.classList.toggle('hide');
         });
-      }
-//---------------------scroll page with nav-----
-  
-
+      }  
+}
 
 
